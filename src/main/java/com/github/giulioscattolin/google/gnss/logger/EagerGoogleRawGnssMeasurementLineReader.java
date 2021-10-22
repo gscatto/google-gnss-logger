a1714a04c86dac7ff50ad2e3c2ec37a62f008ee3
Giulio Scattolin <giulio.scattolin@gmail.com>
2021-10-22 11:43:32 +0200

    refactor: define a readX() method for each field

--- src/main/java/com/github/giulioscattolin/google/gnss/logger/EagerGoogleRawGnssMeasurementLineReader.java
+++ src/main/java/com/github/giulioscattolin/google/gnss/logger/EagerGoogleRawGnssMeasurementLineReader.java
@@ -30,46 +30,190 @@ public class EagerGoogleRawGnssMeasurementLineReader extends GoogleRawGnssMeasur
         }
 
         private void read() {
+            readUtcTimeMillis();
+            readTimeNanos();
+            readLeapSecond();
+            readTimeUncertaintyNanos();
+            readFullBiasNanos();
+            readBiasNanos();
+            readBiasUncertaintyNanos();
+            readDriftNanosPerSecond();
+            readDriftUncertaintyNanosPerSecond();
+            readHardwareClockDiscontinuityCount();
+            readSvid();
+            readTimeOffsetNanos();
+            readState();
+            readReceivedSvTimeNanos();
+            readReceivedSvTimeUncertaintyNanos();
+            readCn0DbHz();
+            readPseudorangeRateMetersPerSecond();
+            readPseudorangeRateUncertaintyMetersPerSecond();
+            readAccumulatedDeltaRangeState();
+            readAccumulatedDeltaRangeMeters();
+            readAccumulatedDeltaRangeUncertaintyMeters();
+            readCarrierFrequencyHz();
+            readCarrierCycles();
+            readCarrierPhase();
+            readCarrierPhaseUncertainty();
+            readMultipathIndicator();
+            readSnrInDb();
+            readConstellationType();
+            readAgcDb();
+            readBasebandCn0DbHz();
+            readFullInterSignalBiasNanos();
+            readFullInterSignalBiasUncertaintyNanos();
+            readSatelliteInterSignalBiasNanos();
+            readSatelliteInterSignalBiasUncertaintyNanos();
+            readCodeType();
+            readChipsetElapsedRealtimeNanos();
+        }
+
+        private void readUtcTimeMillis() {
             trySupplyLongAt(1, itsGoogleRawGnssMeasurement::setUtcTimeMillis);
+        }
+
+        private void readTimeNanos() {
             trySupplyLongAt(2, itsGoogleRawGnssMeasurement::setTimeNanos);
+        }
+
+        private void readLeapSecond() {
             trySupplyIntegerAt(3, itsGoogleRawGnssMeasurement::setLeapSecond);
+        }
+
+        private void readTimeUncertaintyNanos() {
             trySupplyDoubleAt(4, itsGoogleRawGnssMeasurement::setTimeUncertaintyNanos);
+        }
+
+        private void readFullBiasNanos() {
             trySupplyLongAt(5, itsGoogleRawGnssMeasurement::setFullBiasNanos);
+        }
+
+        private void readBiasNanos() {
             trySupplyDoubleAt(6, itsGoogleRawGnssMeasurement::setBiasNanos);
+        }
+
+        private void readBiasUncertaintyNanos() {
             trySupplyDoubleAt(7, itsGoogleRawGnssMeasurement::setBiasUncertaintyNanos);
+        }
+
+        private void readDriftNanosPerSecond() {
             trySupplyDoubleAt(8, itsGoogleRawGnssMeasurement::setDriftNanosPerSecond);
+        }
+
+        private void readDriftUncertaintyNanosPerSecond() {
             trySupplyDoubleAt(9, itsGoogleRawGnssMeasurement::setDriftUncertaintyNanosPerSecond);
+        }
+
+        private void readHardwareClockDiscontinuityCount() {
             trySupplyIntegerAt(10, itsGoogleRawGnssMeasurement::setHardwareClockDiscontinuityCount);
+        }
+
+        private void readSvid() {
             trySupplyIntegerAt(11, itsGoogleRawGnssMeasurement::setSvid);
+        }
+
+        private void readTimeOffsetNanos() {
             trySupplyDoubleAt(12, itsGoogleRawGnssMeasurement::setTimeOffsetNanos);
+        }
+
+        private void readState() {
             trySupplyIntegerAt(13, itsGoogleRawGnssMeasurement::setState);
+        }
+
+        private void readReceivedSvTimeNanos() {
             trySupplyLongAt(14, itsGoogleRawGnssMeasurement::setReceivedSvTimeNanos);
+        }
+
+        private void readReceivedSvTimeUncertaintyNanos() {
             trySupplyLongAt(15, itsGoogleRawGnssMeasurement::setReceivedSvTimeUncertaintyNanos);
+        }
+
+        private void readCn0DbHz() {
             trySupplyDoubleAt(16, itsGoogleRawGnssMeasurement::setCn0DbHz);
+        }
+
+        private void readPseudorangeRateMetersPerSecond() {
             trySupplyDoubleAt(17, itsGoogleRawGnssMeasurement::setPseudorangeRateMetersPerSecond);
+        }
+
+        private void readPseudorangeRateUncertaintyMetersPerSecond() {
             trySupplyDoubleAt(18, itsGoogleRawGnssMeasurement::setPseudorangeRateUncertaintyMetersPerSecond);
+        }
+
+        private void readAccumulatedDeltaRangeState() {
             trySupplyIntegerAt(19, itsGoogleRawGnssMeasurement::setAccumulatedDeltaRangeState);
+        }
+
+        private void readAccumulatedDeltaRangeMeters() {
             trySupplyDoubleAt(20, itsGoogleRawGnssMeasurement::setAccumulatedDeltaRangeMeters);
+        }
+
+        private void readAccumulatedDeltaRangeUncertaintyMeters() {
             trySupplyDoubleAt(21, itsGoogleRawGnssMeasurement::setAccumulatedDeltaRangeUncertaintyMeters);
+        }
+
+        private void readCarrierFrequencyHz() {
             trySupplyDoubleAt(22, itsGoogleRawGnssMeasurement::setCarrierFrequencyHz);
+        }
+
+        private void readCarrierCycles() {
             trySupplyLongAt(23, itsGoogleRawGnssMeasurement::setCarrierCycles);
+        }
+
+        private void readCarrierPhase() {
             trySupplyDoubleAt(24, itsGoogleRawGnssMeasurement::setCarrierPhase);
+        }
+
+        private void readCarrierPhaseUncertainty() {
             trySupplyDoubleAt(25, itsGoogleRawGnssMeasurement::setCarrierPhaseUncertainty);
+        }
+
+        private void readMultipathIndicator() {
             trySupplyIntegerAt(26, itsGoogleRawGnssMeasurement::setMultipathIndicator);
+        }
+
+        private void readSnrInDb() {
             trySupplyDoubleAt(27, itsGoogleRawGnssMeasurement::setSnrInDb);
+        }
+
+        private void readConstellationType() {
             trySupplyIntegerAt(28, itsGoogleRawGnssMeasurement::setConstellationType);
+        }
+
+        private void readAgcDb() {
             trySupplyDoubleAt(29, itsGoogleRawGnssMeasurement::setAgcDb);
+        }
+
+        private void readBasebandCn0DbHz() {
             trySupplyDoubleAt(30, itsGoogleRawGnssMeasurement::setBasebandCn0DbHz);
+        }
+
+        private void readFullInterSignalBiasNanos() {
             trySupplyDoubleAt(31, itsGoogleRawGnssMeasurement::setFullInterSignalBiasNanos);
+        }
+
+        private void readFullInterSignalBiasUncertaintyNanos() {
             trySupplyDoubleAt(32, itsGoogleRawGnssMeasurement::setFullInterSignalBiasUncertaintyNanos);
+        }
+
+        private void readSatelliteInterSignalBiasNanos() {
             trySupplyDoubleAt(33, itsGoogleRawGnssMeasurement::setSatelliteInterSignalBiasNanos);
+        }
+
+        private void readSatelliteInterSignalBiasUncertaintyNanos() {
             trySupplyDoubleAt(34, itsGoogleRawGnssMeasurement::setSatelliteInterSignalBiasUncertaintyNanos);
+        }
+
+        private void readCodeType() {
             trySupplyStringAt(35, itsGoogleRawGnssMeasurement::setCodeType);
+        }
+
+        private void readChipsetElapsedRealtimeNanos() {
             trySupplyLongAt(36, itsGoogleRawGnssMeasurement::setChipsetElapsedRealtimeNanos);
         }
 
         private void trySupplyLongAt(int index, Consumer<Long> consumer) {
-            if (!isFieldEmpty(index))
+            if (isFieldNonEmpty(index))
                 supplyLongAt(index, consumer);
         }
 
@@ -81,7 +225,7 @@ public class EagerGoogleRawGnssMeasurementLineReader extends GoogleRawGnssMeasur
         }
 
         private void trySupplyIntegerAt(int index, Consumer<Integer> consumer) {
-            if (!isFieldEmpty(index))
+            if (isFieldNonEmpty(index))
                 supplyIntegerAt(index, consumer);
         }
 
@@ -93,7 +237,7 @@ public class EagerGoogleRawGnssMeasurementLineReader extends GoogleRawGnssMeasur
         }
 
         private void trySupplyDoubleAt(int index, Consumer<Double> consumer) {
-            if (!isFieldEmpty(index))
+            if (isFieldNonEmpty(index))
                 supplyDoubleAt(index, consumer);
         }
 
@@ -105,7 +249,7 @@ public class EagerGoogleRawGnssMeasurementLineReader extends GoogleRawGnssMeasur
         }
 
         private void trySupplyStringAt(int index, Consumer<String> consumer) {
-            if (!isFieldEmpty(index))
+            if (isFieldNonEmpty(index))
                 supplyStringAt(index, consumer);
         }
 
@@ -125,8 +269,8 @@ public class EagerGoogleRawGnssMeasurementLineReader extends GoogleRawGnssMeasur
             return parseDouble(getFieldAt(index));
         }
 
-        private boolean isFieldEmpty(int index) {
-            return getFieldAt(index).isEmpty();
+        private boolean isFieldNonEmpty(int index) {
+            return !getFieldAt(index).isEmpty();
         }
 
         private String getFieldAt(int index) {
